2884c4d4b2150726409239e15a2bd4926726b974
Bylvin <56466617+Bylvin@users.noreply.github.com>
2022-11-01 13:49:05 +0700

    Buat inetrface dao

---
+++ ionicExercise/src/app/services/dao.ts
@@ -0,0 +1,10 @@
+export interface Dao<T>
+{
+  all(): Promise<T[]>;
+  clear();
+  delete(params: object);
+  findOne(params: object): Promise<T>;
+  insertOrUpdate(model: T);
+  get(params: object): Promise<T[]>;
+  repository();
+}
